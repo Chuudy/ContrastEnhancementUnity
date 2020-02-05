f9984385feec14d8b5d832d6a6d60efd6bf8880a
unknown <user@lap-14-81.mpi-inf.mpg.de>
2020-02-05 13:33:09 +0100

    base

---
+++ GaussianPyramid.cs
@@ -0,0 +1,264 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class GaussianPyramid : MonoBehaviour
+{
+    public enum DebugEnum
+    {
+        NoDebug,
+        GaussianPyramid,
+        LaPlacePyramid
+    }
+
+    public enum GaussPyramidEnum
+    {
+        Downsampling,
+        FastGausian
+    }
+
+    public enum HMD
+    {
+        OculusRiftCV1
+    }
+
+    [Header("Sahders")]
+    public Shader gaussianShader;
+    public Shader kulikowskiBoostShader;
+
+    [Header("HMD Setup")]
+    public HMD hmd;
+
+    [Header("Algorith parameters")]
+    [Range(1, 10)]
+    public int levels = 10;
+    public GaussPyramidEnum gaussianPyramidMode;
+    [Range(0.001f, 300)]
+    public float luminancePhysical = 8;
+    [Range(0.001f, 300)]
+    public float luminanacToMimic = 80;
+    [Range(0.01f, 4)]
+    public float rhoMultiplier = 1;
+
+    [Header("Debug parameters")]
+    public DebugEnum debugMode;
+    [Range(1, 10)]
+    public int previewLevel = 1;
+
+
+    private float resolutionPpd = 0;
+
+    private RenderTexture[] gaussianPyramid = new RenderTexture[10];
+    private RenderTexture[] laPlacePyramid = new RenderTexture[10];
+
+    private Material gausianPyramidMat;
+    private Material kulikowskiBoostMat;
+
+    private const int GausianBlurPassX = 3;
+    private const int GausianBlurPassY = 4;
+    private const int RGBtoYPass = 5;
+    private const int LaPlaceDiffPass = 6;
+    private const int LaPlaceSumPass = 7;
+    private const int YtoRGBPass = 8;
+    private const int BoxDownPass = 0;
+    private const int BoxUpPass = 1;
+    private const int DiffPass = 2;
+
+    private bool toggle = true;
+
+    RenderTexture level0, levelX_temp, levelX;
+
+    private void OnDestroy()
+    {
+        RenderTexture.ReleaseTemporary(levelX_temp);
+        for (int i = 0; i < levels; ++i)
+        {
+            RenderTexture.ReleaseTemporary(gaussianPyramid[i]);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+            toggle = !toggle;
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if(!toggle)
+        {
+            Graphics.Blit(source, destination);
+            return;
+        }
+
+        // Init Materials if don't exist
+        if (gausianPyramidMat == null)
+        {
+            gausianPyramidMat = new Material(gaussianShader)
+            {
+                hideFlags = HideFlags.HideAndDontSave
+            };
+        }
+        if (kulikowskiBoostMat == null)
+        {
+            kulikowskiBoostMat = new Material(kulikowskiBoostShader)
+            {
+                hideFlags = HideFlags.HideAndDontSave
+            };
+        }
+
+        //Init Textures
+        if(level0 == null)
+        {
+            level0 = RenderTexture.GetTemporary(source.width, source.height, 0);
+            levelX_temp = RenderTexture.GetTemporary(source.width, source.height, 0);
+            for (int i = 1; i < levels; ++i)
+            {
+                levelX = RenderTexture.GetTemporary(source.width, source.height, 0);
+                gaussianPyramid[i] = levelX;
+            }
+        }
+
+        // Init resolution
+        if (hmd == HMD.OculusRiftCV1)
+            resolutionPpd = 12;
+
+        // Convert RGV to Y AND
+        // Create level0 of the Gaussian pyramid and store it in the array
+        Graphics.Blit(source, level0, gausianPyramidMat, RGBtoYPass);
+        gaussianPyramid[0] = level0;
+
+        // Genearting levels of gaussian pyramid
+        if (gaussianPyramidMode == GaussPyramidEnum.Downsampling)
+        for (int i = 1; i < levels; ++i)
+        {
+            int scale = (int)Mathf.Pow(2f, i);
+            RenderTexture levelX = RenderTexture.GetTemporary(gaussianPyramid[i - 1].width / 2, gaussianPyramid[i - 1].height / 2, 0);
+            Graphics.Blit(gaussianPyramid[i-1], levelX);
+            gaussianPyramid[i] = levelX;
+        }
+        else if(gaussianPyramidMode == GaussPyramidEnum.FastGausian)
+        {
+            for (int i = 1; i < levels; ++i)
+            {
+                float jump = Mathf.Pow(2f, (float)(i-1));
+                gausianPyramidMat.SetFloat("_Jump", jump);
+                RenderTexture levelX = gaussianPyramid[i];
+                Graphics.Blit(gaussianPyramid[i - 1], levelX_temp, gausianPyramidMat, GausianBlurPassX);  // Blur X
+                Graphics.Blit(levelX_temp, levelX, gausianPyramidMat, GausianBlurPassY);  // Blur Y
+                gaussianPyramid[i] = levelX;
+            }
+        }
+
+        // Converting Gaussian Pyramid to LaPlace pyramid leaving base level as it is
+        for (int i = 0; i < levels - 1; ++i)
+        {
+            RenderTexture temp = RenderTexture.GetTemporary(source.width, source.height, 0, RenderTextureFormat.ARGBFloat);
+            gausianPyramidMat.SetTexture("_SourceTex", gaussianPyramid[i + 1]);
+            Graphics.Blit(gaussianPyramid[i], temp, gausianPyramidMat, LaPlaceDiffPass);
+            laPlacePyramid[i] = temp;
+        }
+        RenderTexture baseLevel = RenderTexture.GetTemporary(source.width, source.height, 0);
+        Graphics.Blit(gaussianPyramid[levels-1], baseLevel);
+        laPlacePyramid[levels-1] = baseLevel;
+
+        // Kulikowski Contrast Boost
+        kulikowskiBoostMat.SetTexture("_BaseLevelTex", laPlacePyramid[levels - 1]);
+        kulikowskiBoostMat.SetFloat("_LIn", luminancePhysical);
+        kulikowskiBoostMat.SetFloat("_LOut", luminanacToMimic);
+        RenderTexture tempTex = RenderTexture.GetTemporary(source.width, source.height, 0, RenderTextureFormat.ARGBHalf);
+        for(int i = 0; i < levels - 1; ++i)
+        {
+            float rho = resolutionPpd * Mathf.Pow(2f, -(1 + i)); 
+            kulikowskiBoostMat.SetFloat("_Rho", rho * rhoMultiplier);
+            Graphics.Blit(laPlacePyramid[i], tempTex, kulikowskiBoostMat);
+            Graphics.Blit(tempTex, laPlacePyramid[i]);
+        }
+        RenderTexture.ReleaseTemporary(tempTex);
+
+        //Summing back the boosted frequncy bands to get Y OUT
+        RenderTexture yOut = RenderTexture.GetTemporary(source.width, source.height, 0, RenderTextureFormat.ARGBHalf);
+        Graphics.Blit(laPlacePyramid[levels - 1], yOut);
+        tempTex = RenderTexture.GetTemporary(source.width, source.height, 0, RenderTextureFormat.ARGBHalf);
+        for (int i = 0; i < levels - 1; ++i)
+        {
+            gausianPyramidMat.SetTexture("_SourceTex", laPlacePyramid[i]);
+            Graphics.Blit(yOut, tempTex, gausianPyramidMat, LaPlaceSumPass);
+            Graphics.Blit(tempTex, yOut);
+        }
+        RenderTexture.ReleaseTemporary(tempTex);
+
+        //Converting grayscale to RGB
+        tempTex = RenderTexture.GetTemporary(source.width, source.height, 0, RenderTextureFormat.ARGBHalf);
+        Graphics.Blit(yOut, tempTex);
+        gausianPyramidMat.SetTexture("_SourceTex", tempTex);
+        Graphics.Blit(source, yOut, gausianPyramidMat, YtoRGBPass);
+        RenderTexture.ReleaseTemporary(tempTex);
+
+
+        // Render out the final outcome or debug
+        if (previewLevel > levels)
+            previewLevel = levels;
+
+        if (debugMode == DebugEnum.GaussianPyramid)
+        {
+            Graphics.Blit(gaussianPyramid[previewLevel - 1], destination);
+        }
+        else if(debugMode == DebugEnum.LaPlacePyramid)
+        {
+            Graphics.Blit(laPlacePyramid[previewLevel - 1], destination);
+        }
+        else if (debugMode == DebugEnum.NoDebug)
+        {
+            Graphics.Blit(yOut, destination);
+        }
+
+        // Clean up
+        RenderTexture.ReleaseTemporary(yOut);
+        for (int i = 0; i < levels; ++i)
+        {
+            RenderTexture.ReleaseTemporary(laPlacePyramid[i]);
+        }
+
+
+        //RenderTexture oneDimGaussian = RenderTexture.GetTemporary(source.width, source.height, 0);
+        //Graphics.Blit(source, oneDimGaussian, gausianPyramid, GausianBlurPassX);
+        //Graphics.Blit(oneDimGaussian, destination, gausianPyramid, GausianBlurPassX);
+        //RenderTexture.ReleaseTemporary(oneDimGaussian);
+
+
+        //int width = source.width / 2;
+        //int height = source.height / 2;
+
+        //RenderTexture currentDestination = textures[0] = RenderTexture.GetTemporary(width, height, 0);
+
+        //Graphics.Blit(source, currentDestination, gausianPyramid, BoxDownPass);
+        //RenderTexture currentSource = currentDestination;
+
+        //int i = 1;
+        //for (; i<levels; ++i)
+        //{
+        //    width /= 2;
+        //    height /= 2;
+        //    if (height < 2)
+        //        break;
+        //    currentDestination = textures[i] = RenderTexture.GetTemporary(width, height, 0);
+        //    Graphics.Blit(currentSource, currentDestination, gausianPyramid, BoxDownPass);
+        //    currentSource = currentDestination;
+        //}
+
+        //for (i -= 2; i >= 0; i--)
+        //{
+        //    currentDestination = textures[i];
+        //    textures[i] = null;
+        //    Graphics.Blit(currentSource, currentDestination, gausianPyramid, BoxUpPass);
+        //    RenderTexture.ReleaseTemporary(currentSource);
+        //    currentSource = currentDestination;
+        //}
+
+        //gausianPyramid.SetTexture("_SourceTex", source);
+        //Graphics.Blit(currentSource, destination, gausianPyramid, DiffPass);
+        //RenderTexture.ReleaseTemporary(currentSource);
+    }
+}
---
+++ GaussianPyramid.shader
@@ -0,0 +1,211 @@
+﻿Shader "Custom/Blur" {
+	Properties{
+		_MainTex("Texture", 2D) = "white" {}
+		_SourceTex("Texture", 2D) = "white" {}
+		_Jump("Jump", Range(0, 1024)) = 0
+	}
+
+		CGINCLUDE
+			#include "UnityCG.cginc"
+
+			sampler2D _MainTex, _SourceTex;
+			float4 _MainTex_TexelSize;
+			float _Jump;
+
+			struct VertexData {
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct Interpolators {
+				float4 pos : SV_POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			Interpolators VertexProgram(VertexData v) {
+				Interpolators i;
+				i.pos = UnityObjectToClipPos(v.vertex);
+				i.uv = v.uv;
+				return i;
+			}
+
+			half3 Sample(float2 uv) {
+				return tex2D(_MainTex, uv).rgb;
+			}
+
+			half3 SampleBox(float2 uv, float delta) {
+				float4 o = _MainTex_TexelSize.xyxy * float2(-delta, delta).xxyy;
+				half3 s =
+					Sample(uv + o.xy) + Sample(uv + o.zy) +
+					Sample(uv + o.xw) + Sample(uv + o.zw);
+				return s * 0.25f;
+			}
+
+			half3 SampleGauss(float2 uv, float jump, float2 dim) {
+				// K = [0.05, 0.25, 0.40, 0.25, 0.05];
+				float2 o = _MainTex_TexelSize.xy * float2(jump, jump) * dim;
+				half3 s =
+					Sample(uv - o * 2) * 0.05 +
+					Sample(uv - o * 1) * 0.25 +
+					Sample(uv) * 0.40 +
+					Sample(uv + o * 1) * 0.25 +
+					Sample(uv + o * 2) * 0.05;;
+				return s;
+			}
+
+			float Remap(float x, float minIn, float maxIn, float minOut, float maxOut)
+			{
+				return (x - minIn) / (maxIn - minIn)*(maxOut - minOut) + minOut;
+			}
+
+			float3 rgb2Yuv(float3 rgb)
+			{
+				float3x3 m_rgb2yuv = float3x3(	0.299f, 0.587f, 0.114f, 
+												-0.147f, -0.289f, 0.437f, 
+												0.615f, -0.515f, -0.100f);
+				float3 yuv = mul(m_rgb2yuv,rgb);
+				return yuv;
+			}
+
+			float3 yuv2rgb(float3 yuv)
+			{
+				float3x3 m_yuv2rgb = float3x3(	1.000f, 0.000f, 1.13983f,
+												1.000f, -0.39465f, -0.58060f,
+												1.000f, 2.03211f, 0.000f);
+				float3 rgb = mul(m_yuv2rgb, yuv);
+				return rgb;
+			}
+
+		ENDCG
+
+
+		SubShader{
+			Cull Off
+			ZTest Always
+			ZWrite Off
+			
+			Pass { 
+				// Downsample pass 0
+				CGPROGRAM
+					#pragma vertex VertexProgram
+					#pragma fragment FragmentProgram
+				
+					half4 FragmentProgram(Interpolators i) : SV_Target {
+						return half4(SampleBox(i.uv, 1), 1);
+					}
+				ENDCG
+			}
+
+			Pass {
+				// Upsample pass 1
+				CGPROGRAM
+					#pragma vertex VertexProgram
+					#pragma fragment FragmentProgram
+
+					half4 FragmentProgram(Interpolators i) : SV_Target {
+						return half4(SampleBox(i.uv, 0.5), 1);
+					}
+				ENDCG
+			}
+
+			Pass {
+				// Difference pass 2
+				CGPROGRAM
+					#pragma vertex VertexProgram
+					#pragma fragment FragmentProgram
+
+					half4 FragmentProgram(Interpolators i) : SV_Target {
+						half4 c = tex2D(_SourceTex, i.uv);
+						c.rgb += SampleBox(i.uv, 0.5);
+						return c;
+					}
+				ENDCG
+			}
+
+			Pass {
+				// Gaussian Blur X pass 3
+				CGPROGRAM
+					#pragma vertex VertexProgram
+					#pragma fragment FragmentProgram
+				
+					half4 FragmentProgram(Interpolators i) : SV_Target {
+						return half4(SampleGauss(i.uv, _Jump, float2(1,0)), 1);
+					}
+				ENDCG
+			}
+
+			Pass {
+				// Gaussian Blur Y pass 4
+				CGPROGRAM
+					#pragma vertex VertexProgram
+					#pragma fragment FragmentProgram
+
+					half4 FragmentProgram(Interpolators i) : SV_Target {
+						return half4(SampleGauss(i.uv, _Jump, float2(0,1)), 1);
+					}
+				ENDCG
+				}
+
+			Pass {
+				// RGB to luminance pass 5
+				CGPROGRAM
+					#pragma vertex VertexProgram
+					#pragma fragment FragmentProgram
+
+					half4 FragmentProgram(Interpolators i) : SV_Target {
+						/*half3 y = dot(Sample(i.uv), half3(0.212656, 0.715158, 0.072186));
+						return clamp(half4(y, 1),0,1);*/
+
+						float y = rgb2Yuv(Sample(i.uv)).r;
+						return clamp(half4(y, y, y, 1), 0, 1);
+					}
+				ENDCG
+				}
+			
+			Pass {
+				// LaPlace Difference 6
+				CGPROGRAM
+					#pragma vertex VertexProgram
+					#pragma fragment FragmentProgram
+
+					half4 FragmentProgram(Interpolators i) : SV_Target {
+						half3 c0 = Sample(i.uv);
+						half3 c1 = tex2D(_SourceTex, i.uv).rgb;						
+						return half4(c0 - c1, 1);
+					}
+				ENDCG
+				}
+
+			Pass {
+				// LaPlace Sumation 7
+				CGPROGRAM
+					#pragma vertex VertexProgram
+					#pragma fragment FragmentProgram
+
+					half4 FragmentProgram(Interpolators i) : SV_Target {
+						half3 c0 = Sample(i.uv);
+						half3 c1 = tex2D(_SourceTex, i.uv).rgb;
+						return half4(c0 + c1, 1);
+					}
+				ENDCG
+				}
+
+			Pass{
+				// Y to RGB conversion Sumation 8
+				CGPROGRAM
+					#pragma vertex VertexProgram
+					#pragma fragment FragmentProgram
+
+					half4 FragmentProgram(Interpolators i) : SV_Target {
+						half3 rgbIn = Sample(i.uv);
+						half3 yuvIn = rgb2Yuv(rgbIn);
+						half yOut = tex2D(_SourceTex, i.uv).r;
+						half3 rgbOut = yuv2rgb(float3(yOut, yuvIn.g, yuvIn.b));
+
+						/*return half4(yuvIn.r, yuvIn.g, yuvIn.b, 1);*/
+						return half4(rgbOut, 1);
+					}
+				ENDCG
+				}
+	}
+}
