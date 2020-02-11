d65bb062cf30bb39193239e768a5fc8c077d1663
Krzysztof Wolski <kw514@ad.cl.cam.ac.uk>
2020-02-11 16:04:32 +0000

    90 FPS contrast enhancement implemented

--- ContrastEnhancement.cs
+++ ContrastEnhancement.cs
@@ -6,12 +6,27 @@ using Valve.VR;
 [ExecuteInEditMode, ImageEffectAllowedInSceneView]
 public class ContrastEnhancement : MonoBehaviour
 {
-    public Shader testShader;
-    private Material testMaterial;
+    enum RenderPass
+    {
+        MAIN,
+        RGB2YUVL,
+        YUVL2L
+    }
+
+    public Shader contrastEnhancementShader;
+    private Material contrastEnhancementMaterial;
 
     public bool toggle = true;
 
-    public SteamVR_Input_Sources rightHand = SteamVR_Input_Sources.RightHand;
+    [Header("Algorith parameters")]
+    [Range(0.001f, 300)]
+    public float luminanceTarget = 8;
+    [Range(0.001f, 300)]
+    public float luminanceSource = 80;
+    [Range(0.01f, 4)]
+    public float rhoMultiplier = 1;
+
+    private SteamVR_Input_Sources rightHand = SteamVR_Input_Sources.RightHand;
 
     private void Start()
     {
@@ -38,21 +53,37 @@ public class ContrastEnhancement : MonoBehaviour
             return;
         }
 
-        if (testMaterial == null)
+        if (contrastEnhancementMaterial == null)
         {
-            testMaterial = new Material(testShader)
+            contrastEnhancementMaterial = new Material(contrastEnhancementShader)
             {
                 hideFlags = HideFlags.HideAndDontSave
             };
         }
-        testMaterial.SetFloat("_Jump", 1);
+
+        RenderTexture YUVL = RenderTexture.GetTemporary(source.width, source.height, 0, RenderTextureFormat.ARGBFloat);
+        RenderTexture L = RenderTexture.GetTemporary(source.width, source.height, 0, RenderTextureFormat.RFloat);
+
+        Graphics.Blit(source, YUVL, contrastEnhancementMaterial, (int)RenderPass.RGB2YUVL);
+        Graphics.Blit(YUVL, L, contrastEnhancementMaterial, (int)RenderPass.YUVL2L);
+
+        contrastEnhancementMaterial.SetTexture("_YuvlTex", YUVL);
+        contrastEnhancementMaterial.SetFloat("_LumTarget", luminanceTarget);
+        contrastEnhancementMaterial.SetFloat("_LumSource", luminanceSource);
+
+        Graphics.Blit(L, destination, contrastEnhancementMaterial, (int)RenderPass.MAIN);
+
+        //Graphics.Blit(YUVL, destination);
+
+        RenderTexture.ReleaseTemporary(YUVL);
+        RenderTexture.ReleaseTemporary(L);
 
         //RenderTexture tmp = RenderTexture.GetTemporary(source.width, source.height, 0, RenderTextureFormat.RFloat);
         //Graphics.Blit(source, tmp, testMaterial, 3);
         //Graphics.Blit(tmp, destination, testMaterial, 1);
         //Graphics.Blit(source, destination, testMaterial, 0);
         //Graphics.Blit(source, destination, testMaterial, 1);
-        Graphics.Blit(source, destination, testMaterial, 2);
+        //Graphics.Blit(source, destination, contrastEnhancementMaterial, 2);
         //RenderTexture.ReleaseTemporary(tmp);
     }
 }
