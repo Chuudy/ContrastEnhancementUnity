1df82cbf393668413d425f11d1e994128a281232
unknown <user@lap-14-81.mpi-inf.mpg.de>
2020-03-23 20:20:39 +0100

    Navmesh creation implemented

---
+++ LegacyRP/Navigation/CreateNavmesh.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class CreateNavmesh : MonoBehaviour
+{
+    public GameObject[] navmeshes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navmeshes = GameObject.FindGameObjectsWithTag("Teleport");
+
+        foreach (GameObject respawn in navmeshes)
+        {
+            GameObject instance = Instantiate(respawn, respawn.transform.position+new Vector3(0,0.001f,0), respawn.transform.rotation);
+            instance.AddComponent<Valve.VR.InteractionSystem.TeleportArea>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
