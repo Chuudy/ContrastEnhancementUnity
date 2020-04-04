b02aac2ab58b8f85f932d74a4f50c06804909eb6
unknown <user@lap-14-81.mpi-inf.mpg.de>
2020-04-04 13:29:55 +0200

    navigation scripts ad computeshader added

--- ComputeShader/ContrastEnhancement.cs
+++ ComputeShader/ContrastEnhancement.cs
@@ -12,6 +12,8 @@ public class ContrastEnhancement : MonoBehaviour
 
     float[] kernel5;
     float[] kernel9;
+    float Z5;
+    float Z9;
 
     RenderTexture RT;
     RenderTexture YUVL;
@@ -20,8 +22,11 @@ public class ContrastEnhancement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GenerateKernel(ref kernel5, 5, 2);
-        GenerateKernel(ref kernel9, 5, 4);
+        GenerateKernel(ref kernel5, ref Z5, 5, 2);
+        GenerateKernel(ref kernel9, ref Z9, 5, 4);
+
+        Debug.Log(Z5);
+        Debug.Log(Z9);
 
         RT = new RenderTexture(InputTexture.width, InputTexture.height, 24);
         RT.enableRandomWrite = true;
@@ -67,13 +72,20 @@ public class ContrastEnhancement : MonoBehaviour
         return 0.39894f * Mathf.Exp(-0.5f * x * x / (sigma * sigma)) / sigma;
     }
 
-    void GenerateKernel(ref float[] kernel, int mSize, float sigma)
+    void GenerateKernel(ref float[] kernel, ref float Z, int mSize, float sigma)
     {
+        kernel = new float[mSize];
         int kSize = (mSize - 1) / 2;
-        kernel = new float[kSize];
         for (int j = 0; j <= kSize; ++j)
         {
             kernel[kSize + j] = kernel[kSize - j] = Normpdf((float)j, sigma);
         }
+
+        Z = 0;
+        for (int k = 0; k < mSize; ++k)
+        {
+            Z += kernel[k];
+        }
+        Z = Z * Z;
     }
 }
---
+++ LegacyRP/Navigation/TeleportToNewLocation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class TeleportToNewLocation : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject location1;
+    public GameObject location2;
+
+    bool toggle = false;
+
+    private SteamVR_Input_Sources leftHand = SteamVR_Input_Sources.LeftHand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player.transform.position = location1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SteamVR_Input.GetStateDown("GrabPinch", leftHand))
+        {
+            toggle = !toggle;
+            if (toggle)
+                player.transform.position = location2.transform.position;
+            if (!toggle)
+                player.transform.position = location1.transform.position;
+        }
+
+       
+    }
+}
