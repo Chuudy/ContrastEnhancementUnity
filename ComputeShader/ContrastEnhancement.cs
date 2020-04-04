40b5be91eb39f3937fcc6d2c547a93127936cc3c
unknown <user@lap-14-81.mpi-inf.mpg.de>
2020-04-04 12:52:47 +0200

    navigation scripts ad computeshader added

---
+++ ComputeShader/ContrastEnhancement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ContrastEnhancement : MonoBehaviour
+{
+    public Texture InputTexture;
+    public ComputeShader shader;
+
+    public bool toggle = true;
+
+    float[] kernel5;
+    float[] kernel9;
+
+    RenderTexture RT;
+    RenderTexture YUVL;
+    Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateKernel(ref kernel5, 5, 2);
+        GenerateKernel(ref kernel9, 5, 4);
+
+        RT = new RenderTexture(InputTexture.width, InputTexture.height, 24);
+        RT.enableRandomWrite = true;
+        RT.Create();
+
+        YUVL = new RenderTexture(InputTexture.width, InputTexture.height, 0, RenderTextureFormat.ARGBFloat, RenderTextureReadWrite.Linear);
+        YUVL.enableRandomWrite = true;
+        YUVL.Create();
+
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (toggle)
+        {
+            Graphics.Blit(InputTexture, YUVL);
+
+            int getLuminanceKernelHandle = shader.FindKernel("CSGetLuminance");
+            int blurKernelHandle = shader.FindKernel("CSBlur");
+
+            shader.SetTexture(getLuminanceKernelHandle, "Result", RT);
+            shader.SetTexture(getLuminanceKernelHandle, "YUVL", YUVL);
+            shader.Dispatch(getLuminanceKernelHandle, InputTexture.width / 8, InputTexture.height / 8, 1);
+
+            shader.SetTexture(blurKernelHandle, "Result", RT);
+            shader.SetTexture(blurKernelHandle, "YUVL", YUVL);
+            shader.Dispatch(blurKernelHandle, InputTexture.width / 8, InputTexture.height / 8, 1);
+        }
+
+        else
+            Graphics.Blit(InputTexture, RT);
+
+
+        rend.sharedMaterial.mainTexture = RT;
+    }
+
+    float Normpdf(float x, float sigma)
+    {
+        return 0.39894f * Mathf.Exp(-0.5f * x * x / (sigma * sigma)) / sigma;
+    }
+
+    void GenerateKernel(ref float[] kernel, int mSize, float sigma)
+    {
+        int kSize = (mSize - 1) / 2;
+        kernel = new float[kSize];
+        for (int j = 0; j <= kSize; ++j)
+        {
+            kernel[kSize + j] = kernel[kSize - j] = Normpdf((float)j, sigma);
+        }
+    }
+}
